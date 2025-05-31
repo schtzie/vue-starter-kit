d34dff7d3311b627a9f7cb9b4ead5116e556024c
Schatzie <dwi.cahyo.mulyo.nugroho@gmail.com>
2025-05-31 20:01:12 +0700

    Update Inertia Request Middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
 
@@ -41,7 +42,11 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'name' => config('app.name'),
             'auth' => [
-                'user' => $request->user(),
+                'user' => Auth::check()
+                    ? collect($request->user())->merge([
+                        'avatar' => $request->user()?->profileAvatar,
+                    ])
+                    : null,
             ],
             'ziggy' => [
                 ...(new Ziggy)->toArray(),
